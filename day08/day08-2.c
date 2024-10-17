34b2a8db60c47ac2c075beb113b67f6ba4862768
gkfls1208 <gkfls1208hr@naver.com>
2024-10-17 15:37:35 +0900

    Update day08-2.c

--- day08/day08-2.c
+++ day08/day08-2.c
@@ -23,18 +23,18 @@ void fillRandom(int array[SIZE][SIZE]){
 
     for (int i = 0; i<SIZE; i++){
         for (int j = 0; j<SIZE; j++){
-            array[i][i] = rand()%20+1;
+            array[i][j] = rand() % 20 + 1;
         }
     }
 }
 
 void printArray(int array[SIZE][SIZE]){
 
-    printf("배열 출력 :");
+    printf("배열 출력:\n");
 
     for(int i=0; i<SIZE; i++){
         for(int j=0; j<SIZE; j++){
-            printf("%2d",array[i][j]);
+            printf("%2d ", array[i][j]);
         }
         printf("\n");
     }
@@ -42,13 +42,13 @@ void printArray(int array[SIZE][SIZE]){
 }
 
 void movePointer(void* array){
-    int x=0, y=0;
+    int x = 0, y = 0;
     int steps;
-    //int* ptr = (int*)array;
+    int* ptr = (int*)array;
 
-    while(0){
-        steps = *((int*)array + x * SIZE +y);
-        printf("현재 위치 : (%d, %d), 배열의 값 : %d\n",x,y,steps);
+    while(1){
+        steps = *(ptr + x * SIZE + y);
+        printf("현재 위치: (%d, %d), 배열의 값: %d\n", x, y, steps);
 
         int new_x = x, new_y = y + steps;
 
@@ -57,12 +57,12 @@ void movePointer(void* array){
             new_x++;
         }
 
-        if(new_x>=SIZE){
-            printf("더 이상 이동할 수 없습니다. \n종료 위치 : (%d, %d), 배열의 값 : %d\n", x, y, steps);
+        if(new_x >= SIZE){
+            printf("더 이상 이동할 수 없습니다.\n종료 위치: (%d, %d), 배열의 값: %d\n", x, y, steps);
             break;
         }
 
-        x=new_x;
-        y=new_y;
+        x = new_x;
+        y = new_y;
     }
 }
